b3bec34ff1012544f1016dfb1af5471a76d94598
Nouman <noumankhan95@yahoo.com>
2024-03-15 03:13:31 +0500

    Added Images

--- components/KonvaCard.tsx
+++ components/KonvaCard.tsx
@@ -6,8 +6,18 @@ interface TextBox {
   id: number;
   text: string;
   textAlign: "left" | "center" | "right";
+  fontSize: number;
+  fill: string;
 }
 
+interface CardImage {
+  width: number;
+  height: number;
+  source: CanvasImageSource;
+  id: number;
+  x?: number;
+  y?: number;
+}
 interface BusinessCardProps {
   width: number;
   height: number;
@@ -25,11 +35,10 @@ const BusinessCard: React.FC<BusinessCardProps> = ({
   const [isdownloading, setisdownloading] = useState<boolean>();
   const [forceRender, setForceRender] = useState(false);
   const canvasRef = useRef<any>(null);
-  const [textboxes, setTextboxes] = useState<TextBox[]>([
-    { id: 1, text: "Text 1", textAlign: "left" },
-    { id: 2, text: "Text 2", textAlign: "center" },
-    { id: 3, text: "Text 3", textAlign: "right" },
-  ]);
+  const [textboxes, setTextboxes] = useState<TextBox[]>([]);
+  const [cardImages, setcardImages] = useState<CardImage[]>([]);
+  const cardImageRef = useRef<HTMLInputElement | null>(null);
+  const [editType, seteditType] = useState<string>("");
 
   const handleBackgroundColorChange = (color: string) => {
     setBackgroundColor(color);
@@ -94,103 +103,233 @@ const BusinessCard: React.FC<BusinessCardProps> = ({
       }, 1000);
     }
   };
-  console.log(isdownloading, "downloading");
+  function handleFontChange(id: number, value: number) {
+    setTextboxes((prevState) =>
+      prevState.map((textbox) =>
+        textbox.id === id ? { ...textbox, fontSize: value } : textbox
+      )
+    );
+  }
+  function handleColorChange(id: number, value: string) {
+    setTextboxes((prevState) =>
+      prevState.map((textbox) =>
+        textbox.id === id ? { ...textbox, fill: value } : textbox
+      )
+    );
+  }
+  const handleCardChange = (id: number, type: string, value: string) => {
+    setcardImages((p) =>
+      p.map((c) => (c.id === id ? { ...c, [type]: value } : c))
+    );
+  };
+  console.log(cardImages, "cardimages");
   return (
     <div className="space-y-5">
-      <Stage width={width} height={height} ref={canvasRef}>
-        <Layer>
-          <Group width={width} height={height}>
-            <Rect
-              fill={backgroundColor}
-              width={width - canvasPadding}
-              height={height - canvasPadding}
-            />
-            {backgroundImage && (
-              <Image width={width} height={height} image={backgroundImage} />
-            )}
-            {textboxes.map((textbox) => {
-              console.log(textbox);
-              return (
-                <Text
-                  key={textbox.id}
-                  x={
-                    textbox.textAlign === "left"
-                      ? canvasPadding // Align left
-                      : textbox.textAlign === "center"
-                      ? width / 2 // Align center
-                      : width - canvasPadding * 10 // Align right
-                  }
-                  y={canvasPadding}
-                  text={textbox.text}
-                  align={textbox.textAlign}
-                  fontSize={20}
-                  fill="black"
-                  draggable
-                  onClick={() => {
-                    const newAlignment =
-                      textbox.textAlign === "left"
-                        ? "center"
-                        : textbox.textAlign === "center"
-                        ? "right"
-                        : "left";
-                    handleTextAlignmentChange(textbox.id, newAlignment);
-                  }}
-                  wrap="word"
-                  onDragEnd={(e) => {
-                    if (e.target.x() >= width - canvasPadding) {
-                      console.log("true");
-                      handleTextAlignmentChange(textbox.id, "center");
-                      const newX = width / 2;
-
-                      e.currentTarget.x(newX);
-
-                      e.target?.getLayer()?.batchDraw();
-                    } else if (e.target.x() < canvasPadding) {
-                      console.log("true here");
-                      const newX = width / 2;
-                      e.currentTarget.x(newX);
-                      handleTextAlignmentChange(textbox.id, "center");
-                      e.target?.getLayer()?.batchDraw();
-                    }
-                    if (e.target.y() >= height - canvasPadding) {
-                      console.log("true");
-                      handleTextAlignmentChange(textbox.id, "center");
-                      const newX = height / 2;
-                      e.currentTarget.y(newX);
-
-                      e.target?.getLayer()?.batchDraw();
-                    } else if (e.target.y() < canvasPadding) {
-                      console.log("true here y");
-                      const newX = height / 2;
-                      e.currentTarget.y(newX);
-                      handleTextAlignmentChange(textbox.id, "center");
-                      e.target?.getLayer()?.batchDraw();
-                    }
-                  }}
-                />
-              );
-            })}
-            {backgroundImage && !isdownloading && (
-              <Line
-                points={[
-                  padding + borderPadding, // Index 0: X-coordinate of the top-left corner
-                  padding + borderPadding, // Index 1: Y-coordinate of the top-left corner
-                  width - padding - borderPadding, // Index 2: X-coordinate of the top-right corner
-                  padding + borderPadding, // Index 3: Y-coordinate of the top-right corner
-                  width - padding - borderPadding, // Index 4: X-coordinate of the bottom-right corner
-                  height - padding - borderPadding, // Index 5: Y-coordinate of the bottom-right corner
-                  padding + borderPadding, // Index 6: X-coordinate of the bottom-left corner
-                  height - padding - borderPadding, // Index 7: Y-coordinate of the bottom-left corner
-                  padding + borderPadding, // Index 8: X-coordinate of the top-left corner (to close the shape)
-                  padding + borderPadding, // Index 9: Y-coordinate of the top-left corner (to close the shape)
-                ]}
-                stroke="black"
-                dash={[5, 5]} // Adjust the dash array for the desired dotted effect
+      <div className="flex flex-row justify-start items-start">
+        <Stage width={width} height={height} ref={canvasRef}>
+          <Layer>
+            <Group width={width} height={height}>
+              <Rect
+                fill={backgroundColor}
+                width={width - canvasPadding}
+                height={height - canvasPadding}
               />
-            )}
-          </Group>
-        </Layer>
-      </Stage>
+              {backgroundImage && (
+                <Image width={width} height={height} image={backgroundImage} />
+              )}
+              {textboxes?.map((textbox) => {
+                console.log(textbox);
+                return (
+                  <Text
+                    key={textbox.id}
+                    x={
+                      textbox.textAlign === "left"
+                        ? canvasPadding // Align left
+                        : textbox.textAlign === "center"
+                        ? width / 2 // Align center
+                        : width - canvasPadding * 4 // Align right
+                    }
+                    y={canvasPadding}
+                    text={textbox.text}
+                    align={textbox.textAlign}
+                    fontSize={textbox.fontSize}
+                    fill={textbox.fill}
+                    draggable
+                    onClick={() => {
+                      const newAlignment =
+                        textbox.textAlign === "left"
+                          ? "center"
+                          : textbox.textAlign === "center"
+                          ? "right"
+                          : "left";
+                      handleTextAlignmentChange(textbox.id, newAlignment);
+                    }}
+                    wrap="word"
+                    onDragEnd={(e) => {
+                      if (e.target.x() >= width - canvasPadding) {
+                        console.log("true");
+                        handleTextAlignmentChange(textbox.id, "center");
+                        const newX = width / 2;
+
+                        e.currentTarget.x(newX);
+
+                        e.target?.getLayer()?.batchDraw();
+                      } else if (e.target.x() < canvasPadding) {
+                        console.log("true here");
+                        const newX = width / 2;
+                        e.currentTarget.x(newX);
+                        handleTextAlignmentChange(textbox.id, "center");
+                        e.target?.getLayer()?.batchDraw();
+                      }
+                      if (e.target.y() >= height - canvasPadding) {
+                        console.log("true");
+                        handleTextAlignmentChange(textbox.id, "center");
+                        const newX = height / 2;
+                        e.currentTarget.y(newX);
+
+                        e.target?.getLayer()?.batchDraw();
+                      } else if (e.target.y() < canvasPadding) {
+                        console.log("true here y");
+                        const newX = height / 2;
+                        e.currentTarget.y(newX);
+                        handleTextAlignmentChange(textbox.id, "center");
+                        e.target?.getLayer()?.batchDraw();
+                      }
+                    }}
+                  />
+                );
+              })}
+              {cardImages &&
+                cardImages.map((i) => (
+                  <Image
+                    width={i.width}
+                    height={i.height}
+                    image={i.source}
+                    key={i.id}
+                    draggable
+                    onDragEnd={(e) => {
+                      if (e.target.x() >= width - canvasPadding) {
+                        console.log("true");
+                        handleCardChange(i.id, "x", (width / 2).toString());
+                        const newX = width / 2;
+
+                        e.currentTarget.x(newX);
+
+                        e.target?.getLayer()?.batchDraw();
+                      } else if (e.target.x() < canvasPadding) {
+                        console.log("true here");
+                        const newX = width / 2;
+                        e.currentTarget.x(newX);
+                        handleCardChange(i.id, "x", (width / 2).toString());
+                        e.target?.getLayer()?.batchDraw();
+                      }
+                      if (e.target.y() >= height - canvasPadding) {
+                        console.log("true");
+                        handleCardChange(i.id, "y", (height / 2).toString());
+                        const newX = height / 2;
+                        e.currentTarget.y(newX);
+
+                        e.target?.getLayer()?.batchDraw();
+                      } else if (e.target.y() < canvasPadding) {
+                        console.log("true here y");
+                        const newX = height / 2;
+                        e.currentTarget.y(newX);
+                        handleCardChange(i.id, "y", (height / 2).toString());
+
+                        e.target?.getLayer()?.batchDraw();
+                      }
+                    }}
+                  />
+                ))}
+              {backgroundImage && !isdownloading && (
+                <Line
+                  points={[
+                    padding + borderPadding, // Index 0: X-coordinate of the top-left corner
+                    padding + borderPadding, // Index 1: Y-coordinate of the top-left corner
+                    width - padding - borderPadding, // Index 2: X-coordinate of the top-right corner
+                    padding + borderPadding, // Index 3: Y-coordinate of the top-right corner
+                    width - padding - borderPadding, // Index 4: X-coordinate of the bottom-right corner
+                    height - padding - borderPadding, // Index 5: Y-coordinate of the bottom-right corner
+                    padding + borderPadding, // Index 6: X-coordinate of the bottom-left corner
+                    height - padding - borderPadding, // Index 7: Y-coordinate of the bottom-left corner
+                    padding + borderPadding, // Index 8: X-coordinate of the top-left corner (to close the shape)
+                    padding + borderPadding, // Index 9: Y-coordinate of the top-left corner (to close the shape)
+                  ]}
+                  stroke="black"
+                  dash={[5, 5]} // Adjust the dash array for the desired dotted effect
+                />
+              )}
+            </Group>
+          </Layer>
+        </Stage>
+        <div className="flex flex-col gap-4">
+          <button
+            className="p-4 bg-blue-600 text-white rounded-md"
+            onClick={(e) => {
+              setTextboxes((p) => [
+                ...(p || []),
+                {
+                  id: (textboxes?.length ?? 0) + 1,
+                  fill: "black",
+                  fontSize: 16,
+                  text: "Enter Text",
+                  textAlign: "center",
+                },
+              ]);
+            }}
+          >
+            Add TextBox
+          </button>
+          <button
+            className="p-4 bg-blue-600 text-white rounded-md"
+            onClick={(e) => {
+              cardImageRef.current?.click();
+            }}
+          >
+            Add Image
+          </button>
+          <input
+            type="file"
+            hidden
+            ref={cardImageRef}
+            onChange={(e) => {
+              const img = new window.Image();
+              img.src = URL.createObjectURL(e.target?.files?.[0]!);
+              img.onload = () => {
+                console.log("loaded");
+
+                setcardImages((prevImages) => [
+                  ...prevImages,
+                  {
+                    width: 130,
+                    height: 190,
+                    source: img,
+                    id: (cardImages.length ?? 0) + 1,
+                  },
+                ]);
+              };
+            }}
+          />
+        </div>
+      </div>
+      <div>
+        <button
+          className="p-4 bg-blue-600 text-white rounded-md"
+          onClick={(e) => {
+            seteditType("text");
+          }}
+        >
+          Edit Text
+        </button>
+        <button
+          className="p-4 bg-blue-600 text-white rounded-md"
+          onClick={(e) => {
+            seteditType("image");
+          }}
+        >
+          Edit Image
+        </button>
+      </div>
       <div>
         <input
           type="color"
@@ -204,28 +343,112 @@ const BusinessCard: React.FC<BusinessCardProps> = ({
             handleBackgroundImageChange(URL.createObjectURL(e.target.files![0]))
           }
         />
-        {textboxes.map((textbox) => (
-          <div key={textbox.id}>
-            <input
-              type="text"
-              value={textbox.text}
-              onChange={(e) => handleTextChange(textbox.id, e.target.value)}
-            />
-            <select
-              value={textbox.textAlign}
-              onChange={(e) =>
-                handleTextAlignmentChange(
-                  textbox.id,
-                  e.target.value as "left" | "center" | "right"
-                )
-              }
+        {editType === "text" &&
+          textboxes.map((textbox) => (
+            <div
+              className="flex flex-row justify-around items-center"
+              key={textbox.id}
             >
-              <option value="left">Left</option>
-              <option value="center">Center</option>
-              <option value="right">Right</option>
-            </select>
-          </div>
-        ))}
+              <div>
+                <input
+                  type="text"
+                  value={textbox.text}
+                  onChange={(e) => handleTextChange(textbox.id, e.target.value)}
+                />
+                <select
+                  value={textbox.textAlign}
+                  onChange={(e) =>
+                    handleTextAlignmentChange(
+                      textbox.id,
+                      e.target.value as "left" | "center" | "right"
+                    )
+                  }
+                >
+                  <option value="left">Left</option>
+                  <option value="center">Center</option>
+                  <option value="right">Right</option>
+                </select>
+              </div>
+              <div>
+                <h1>Change Font Size</h1>
+                <input
+                  type="number"
+                  min={1}
+                  placeholder="Change Text Size"
+                  defaultValue={textbox.fontSize}
+                  onChange={(e) => {
+                    handleFontChange(textbox.id, parseInt(e.target.value));
+                  }}
+                />
+              </div>
+              <div>
+                <h1>Change Font Color</h1>
+
+                <input
+                  type="color"
+                  placeholder="Change Text Color"
+                  defaultValue={textbox.fill}
+                  onChange={(e) => {
+                    handleColorChange(textbox.id, e.target.value);
+                  }}
+                />
+              </div>
+              <div>
+                <h1
+                  className="text-red-700 cursor-pointer"
+                  onClick={(e) => {
+                    setTextboxes((p) => p.filter((i) => i.id !== textbox.id));
+                  }}
+                >
+                  Delete
+                </h1>
+              </div>
+            </div>
+          ))}
+        {editType === "image" &&
+          cardImages?.map((cardimage) => (
+            <div
+              className="flex flex-row justify-around items-center"
+              key={cardimage.id}
+            >
+              <div>
+                <h1>Change Width </h1>
+                <input
+                  type="number"
+                  min={1}
+                  placeholder="Change Text Size"
+                  defaultValue={cardimage.width}
+                  onChange={(e) => {
+                    handleCardChange(cardimage.id, "width", e.target.value);
+                  }}
+                />
+              </div>
+              <div>
+                <h1>Change Height</h1>
+                <input
+                  type="number"
+                  min={1}
+                  placeholder="Change Text Size"
+                  defaultValue={cardimage.height}
+                  onChange={(e) => {
+                    handleCardChange(cardimage.id, "height", e.target.value);
+                  }}
+                />
+              </div>
+              <div>
+                <h1
+                  className="text-red-700 cursor-pointer"
+                  onClick={(e) => {
+                    setcardImages((p) =>
+                      p.filter((i) => i.id !== cardimage.id)
+                    );
+                  }}
+                >
+                  Delete
+                </h1>
+              </div>
+            </div>
+          ))}
         <button
           onClick={() => {
             setisdownloading(true);
